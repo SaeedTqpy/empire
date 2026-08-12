6237d0c1a7a32c928e5483d09e1a236ae47a5755
Saeedtqpy <saeedtqp@gmail.com>
2026-08-12 19:14:54 +0330

    design: load Estedad and Iranian design layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/estedad/wght.css'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './index.css'
 import './terrain.css'
 import './routes.css'
 import './maplibre-terrain.css'
+import './iranian-design.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
